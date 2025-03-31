7a1c9a995a61b64ba12e77aa5c1d31f588e1df36
VIktoriya Cherevatenko <cherevatenkoviktoriya@gmail.com>
2025-03-31 18:56:46 +0300

    fix

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open("chat-cache").then((cache) => {
-            return cache.addAll(["/", "/static/css/style.css", "/static/js/main.js"]);
+            return cache.addAll([
+                "/",
+                "/static/css/style.css", 
+                "/static/js/main.js",
+                "/static/icons/logo-192x192.png",
+                "/static/icons/logo-512x512.png",
+            ]);
         })
     );
 });
