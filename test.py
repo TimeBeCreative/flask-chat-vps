5d725dfc2bb6da9e4efed5b1a8e9dd2f78e693a7
VIktoriya Cherevatenko <cherevatenkoviktoriya@gmail.com>
2025-08-11 17:43:59 +0300

    new day

--- test.py
+++ test.py
@@ -6,18 +6,17 @@ def to_base64url(data: bytes) -> str:
     return base64.urlsafe_b64encode(data).rstrip(b'=').decode('utf-8')
 vapid = Vapid()
 vapid.generate_keys()
-private_key = vapid.private_key
-public_key = vapid.public_key
 
-priv_bytes = private_key.private_bytes(
+
+priv_bytes = vapid.private_key.private_bytes(
     encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.TraditionalOpenSSL,  # PrivateFormat.TraditionalOpenSSL
+    format=serialization.PrivateFormat.PKCS8,  # PrivateFormat.TraditionalOpenSSL
     encryption_algorithm=serialization.NoEncryption()  # NoEncryption()
 )
 
-public_bytes = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo  # PublicFormat.SubjectPublicKeyInfo
+public_bytes = vapid.public_key.public_bytes(
+    encoding=serialization.Encoding.X962,
+    format=serialization.PublicFormat.UncompressedPoint  
 )
 
 print("VAPID_PRIVATE_KEY:", to_base64url(priv_bytes))
